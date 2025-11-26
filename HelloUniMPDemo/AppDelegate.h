435440047c3713faab6a7e5e498d2192b4543319
imcaptor <imcaptor@gmail.com>
2025-11-26 17:42:28 +0800

    Initial commit: UniMP SDK iOS Demo project

---
+++ HelloUniMPDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  HelloUniMPDemo
+//
+//  Created by XHY on 2019/12/28.
+//  Copyright © 2019 DCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
